50597183d54c79c0a6285709d073f365c1591b1f
Lige <lige.williamson@gmail.com>
2018-06-01 15:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function deleteFromObjectByKey(object, key, value) {
   return newObject
 }
 
-function destructivelyDeleteFromObjectByKey(object, key, value) {
-  var object = {key: value}
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
